0e96b33d8076b15cbbc62e0318142f8e6bd1c5a9
ryuyah2000 <ryuyah2000@users.noreply.github.com>
2024-09-17 21:11:25 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+// Calculates the commssion based on the value of the sales
+// <10000 = 10%
+// <15000 = 15%
+// >15000 = 20%
+// code by Ryuya Hirota
+
+#include <iostream>
+
+int main() {
+    double sales;
+    std::cout << "Enter number of sales: " << std::endl;
+    std::cin >> sales;
+    
+    double commission;
+    if (sales < 10000) {
+        commission = sales * 0.1;
+    } else if (sales < 15000) {
+        commission = sales * 0.15;
+    } else {
+        commission = sales * 0.2;
+    }
+    
+    std::cout << "Sales amount: " << sales << std::endl;
+    std::cout << "Commission total: " << commission << std::endl;
+}
